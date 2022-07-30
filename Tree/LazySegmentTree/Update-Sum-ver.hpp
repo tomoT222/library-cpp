26d2be875cfb4a344c64a0e031d820c24810fe17
tomoT222 <65435187+tomoT222@users.noreply.github.com>
2022-07-30 18:13:26 +0900

    Create Update-Sum-ver.hpp

---
+++ Tree/LazySegmentTree/Update-Sum-ver.hpp
@@ -0,0 +1,46 @@
+struct lazyseg {
+	// 遅延評価セグメント木をソラで書きたいあなたに
+	ll nn = 1; vector<ll> node, lazy;// 値配列、遅延配列
+
+	lazyseg(ll n, ll init) {
+		while (nn < n) { nn *= 2; }
+		node.resize(nn * 2, init); lazy.resize(nn * 2, INF);
+	}
+
+	void eval(ll k, ll l, ll r) {// k 番目のノードについて遅延評価を行う
+		// 自ノード及び子ノードへの値の伝播
+		if (lazy[k] != INF) {
+			node[k] = lazy[k];
+			if (r - l > 1) {// 最下段かどうか
+				lazy[2 * k + 1] = lazy[k] / 2;
+				lazy[2 * k + 2] = lazy[k] / 2;
+			}
+			lazy[k] = INF;
+		}
+	}
+
+	void update(ll a, ll b, ll x, ll k = 0, ll l = 0, ll r = -1) { // [a, b), x
+		if (r < 0) { r = nn; }
+		eval(k, l, r);// k 番目のノードに対して遅延評価を行う
+		if (b <= l || r <= a) return; // 範囲外なら何もしない
+		if (a <= l && r <= b) {// 完全に被覆→遅延配列に値を入れた後に評価
+			lazy[k] = (r - l) * x;
+			eval(k, l, r);
+		}
+		else { // そうでないならば、子ノードの値を再帰的に計算して、計算済みの値をもらってくる
+			update(a, b, x, 2 * k + 1, l, (l + r) / 2);
+			update(a, b, x, 2 * k + 2, (l + r) / 2, r);
+			node[k] = node[2 * k + 1] + node[2 * k + 2];
+		} //これで自分の上の頂点の値配列が更新される。
+	}
+
+	ll getsum(ll a, ll b, ll k = 0, ll l = 0, ll r = -1) { // [a, b)
+		if (r < 0) { r = nn; }
+		if (b <= l || r <= a) return 0;
+		eval(k, l, r);// 自分の遅延配列を自分の値配列や子の遅延配列に伝番
+		if (a <= l && r <= b) return node[k];
+		ll vl = getsum(a, b, 2 * k + 1, l, (l + r) / 2);
+		ll vr = getsum(a, b, 2 * k + 2, (l + r) / 2, r);
+		return vl + vr;
+	}
+};
