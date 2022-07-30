15b7aa205e9426f03bb2b89f52a4e32670260754
tomoT222 <65435187+tomoT222@users.noreply.github.com>
2022-07-30 16:22:42 +0900

    Create UnionFind-Weight.hpp

---
+++ Tree/UnionFind-Weight.hpp
@@ -0,0 +1,51 @@
+template<class Abel> struct WeightUnionFind {
+	vector<ll> par;
+	vector<ll> rank;
+	vector<Abel> diff_weight;
+
+	WeightUnionFind(ll n = 1, Abel SUM_UNITY = 0) {
+		init(n, SUM_UNITY);
+	}
+
+	void init(ll n = 1, Abel SUM_UNITY = 0) {
+		par.resize(n); rank.resize(n); diff_weight.resize(n);
+		rep(i, n) {
+			par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
+		}
+	}
+
+	ll root(ll x) {
+		if (par[x] == x) {
+			return x;
+		}
+		else {
+			ll r = root(par[x]);
+			diff_weight[x] += diff_weight[par[x]];
+			return par[x] = r;
+		}
+	}
+
+	Abel weight(ll x) {
+		root(x);
+		return diff_weight[x];
+	}
+
+	bool same(ll x, ll y) {
+		return root(x) == root(y);
+	}
+
+	bool unite(ll x, ll y, Abel w) {
+		w += weight(x); w -= weight(y);
+		x = root(x); y = root(y);
+		if (x == y) return false;
+		if (rank[x] < rank[y]) swap(x, y), w = -w;
+		if (rank[x] == rank[y]) ++rank[x];
+		par[y] = x;
+		diff_weight[y] = w;
+		return true;
+	}
+
+	Abel diff(ll x, ll y) {
+		return weight(y) - weight(x);
+	}
+};
